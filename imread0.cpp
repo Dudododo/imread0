7d8ad3ac26bfe30b3648218c889024b61dffc432
Dudododo <a752601260@gmail.com>
2020-02-28 15:14:29 +0800

    添加项目文件。

---
+++ imread0.cpp
@@ -0,0 +1,13 @@
+﻿#include <opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+	cv::Mat scrMat = imread("C:\\Users\\duzhi\\Pictures\\Saved Pictures\\1.jpg", 0);
+	imshow("ImputImage", scrMat);
+
+	waitKey(0);
+	return 0;
+}
